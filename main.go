98ccaa9aaf24c6a17ad0c2cfcce03d82884bb60c
ehsan-fmb <43479584+ehsan-fmb@users.noreply.github.com>
2019-05-20 05:16:09 +0430

    Update main.go

--- main.go
+++ main.go
@@ -1,43 +1,66 @@
-package main
-
+package testing101
 import (
 	"context"
-	"fmt"
-	"net/http"
 	"time"
-
-	"github.com/labstack/echo"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/labstack/echo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var client mongo.Client
 type test struct {
-	lat string `json:"lat"`
-	lng string `json:"lng"`
+ 	Lat string `json:"lat"`
+	Lng string `json:"lng"`
 }
-
-// JSONHandler handles request json.
-func JSONHandler(c echo.Context) (err error) {
-	t := new(test)
-	if err = c.Bind(t); err == nil {
-		return err
-	}
-
-	client, err := mongo.NewClient(options.Client().ApplyURI("localhost:27017"))
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-
-	collection := client.Database("testing").Collection("lat_lng")
-	res, err := collection.InsertOne(ctx, t)
-	id := res.InsertedID
-	return c.String(http.StatusOK, fmt.Sprintf("man amadam with id: %s", id))
+func JsonHandler(c echo.Context) (err error) {
+  t :=test{}
+  defer c.Request().Body.Close()
+  b,err := ioutil.ReadAll(c.Request().Body)
+  if err!=nil {
+	  log.Printf("failed to rendering request body:%s",err)
+	  return c.String(http.StatusInternalServerError,"")
+  }
+  err=json.Unmarshal(b,&t)
+  if err!=nil {
+          log.Printf("failed to Unmarshaling:%s",err)
+          return c.String(http.StatusInternalServerError,"")
+  }
+  collection := client.Database("testing").Collection("geo")
+  ctx,cancel:= context.WithTimeout(context.Background(), 10*time.Second)
+  res, err := collection.InsertOne(ctx,t)
+  if err!= nil{
+          log.Fatal(err)
+  }
+  id := res.InsertedID
+  defer cancel()
+  return c.String(http.StatusOK, fmt.Sprintf("%s\n%s\n%s\n",id, string(t.Lat),string(t.Lng)))
 }
+func Pinging(c echo.Context) error {
+                return c.String(http.StatusOK, "man amdam")
+        }
 
-func main() {
+//for connect to mongodb
+func join_to_database(){
+	  client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+ 	 if err!= nil{
+         	 log.Fatal(err)
+  	}
+	ctx,cancel:= context.WithTimeout(context.Background(), 10*time.Second)
+  	err = client.Connect(ctx)
+  	if err!= nil{
+          	log.Fatal(err)
+  	}
+	defer cancel()
+}
+func NewServer() {
+	join_to_database()
 	e := echo.New()
-	e.GET("/ping", func(c echo.Context) error {
-		return c.String(http.StatusOK, "")
-	})
-	e.POST("/posting", JSONHandler)
+	e.GET("/ping",Pinging)
+	e.POST("/posting",JsonHandler)
 	e.Logger.Fatal(e.Start(":1323"))
 }
